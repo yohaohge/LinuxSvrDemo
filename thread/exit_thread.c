a7d7bccb0c0bf9407cdd066091d7b8cfbe6184ed
ouzhang <ouzhang@tencent.com>
2020-11-29 10:38:24 +0800

    leak check

--- thread/exit_thread.c
+++ thread/exit_thread.c
@@ -8,8 +8,8 @@ void* deal_func(void* param)
 	// 线程执行函数
 	printf("thread function\n");
 
-	// can not be stack val for retval
-	// stack val will be wrong val pthread_join
+	// 返回不能使用栈内存变量，会导致结果未知
+	// 使用malloc和new申请的内存，需要主要内存释放的问题
 	int* retval = malloc(sizeof(int));
     *retval = 1;	
 	pthread_exit(retval);
@@ -27,8 +27,12 @@ int main()
 
 	int* retval = NULL;
 	pthread_join(thread, (void*)&retval);
-
-	printf("thread %02x retval = %d\n", (unsigned)thread, *retval);
+	//没有处理内存释放问题，有内存泄漏的情况
+	if(retval)
+	{
+		printf("thread %02x retval = %d\n", (unsigned)thread, *retval);
+		free(retval);
+	}
 
 	return 0;
 }
